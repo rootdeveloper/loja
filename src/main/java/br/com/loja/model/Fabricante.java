2da6610f1c58e8c29d3606aaebe22c5dc6d32660
Karoline Viana <kvianavieira@hotmail.com>
2020-05-11 20:38:15 -0300

    adicionando a tela de fabricante

--- src/main/java/br/com/loja/model/Fabricante.java
+++ src/main/java/br/com/loja/model/Fabricante.java
@@ -1,6 +1,5 @@
 package br.com.loja.model;
 
-import java.util.Collection;
 import java.util.List;
 
 
@@ -38,5 +37,44 @@ public class Fabricante extends Empresa {
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codfabricante == null) ? 0 : codfabricante.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((produtos == null) ? 0 : produtos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (codfabricante == null) {
+			if (other.codfabricante != null)
+				return false;
+		} else if (!codfabricante.equals(other.codfabricante))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (produtos == null) {
+			if (other.produtos != null)
+				return false;
+		} else if (!produtos.equals(other.produtos))
+			return false;
+		return true;
+	}
+	
+	
 
 }
